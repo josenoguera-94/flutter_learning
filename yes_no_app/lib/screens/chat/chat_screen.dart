d6c07c55737549ec5b13e91c36fde8482fa4acd2
jose noguera <josenoguera1994@gmail.com>
2024-03-15 22:49:48 -0500

    feat: cap 69 ListView área de los mensajes

--- yes_no_app/lib/screens/chat/chat_screen.dart
+++ yes_no_app/lib/screens/chat/chat_screen.dart
@@ -17,12 +17,47 @@ class ChatScreen extends StatelessWidget {
                 'https://tiermaker.com/images/chart/chart/league-of-legends-2020-todos-os-champions-538139/teemopng.png'),
           ),
         ),
-        title: const Text('Chat'),
-        centerTitle: true,
+        title: const Text('Elon Musk'),
+        centerTitle: false,
       ),
-      body: const Center(
-        child: Text('Chat Screen'),
-      ),
+      // body: Container() es como un div en html
+      body: _ChatView()
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea( // SafeArea es un widget que permite que el contenido no se superponga con la barra de estado
+      // left: false, right: false, bottom: false, top: false
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0), // EdgeInsets.symmetric() crea un padding simétrico horizontal y/o vertical
+          child: Column(
+            children: [
+              Expanded( // Expanded es un widget que permite que un widget hijo ocupe todo el espacio disponible
+                child: ListView.builder( // ListView.builder es un widget que permite crear una lista de elementos de manera eficiente
+                // listView.builder() es útil para listas largas, ya que solo renderiza los elementos que son visibles
+                // funciona bajo demanda, a medida que el usuario se desplaza por la lista
+                // otras opciones son ListView.separated() y ListView.custom() ...
+
+                // itemBuilder recive (context, index) {} es una función que recibe el contexto, que es el
+                // arbol de widgets y el índice del elemento a renderizar, que listView.builder() está utilizando
+                // o (context, index) => Widget() recive el contexto y el índice y retorna un widget
+                  itemCount: 10, // cantidad de elementos a renderizar si no se especifica será infinito
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text( 'Hola mundo $index' );
+                  }
+                )
+                // child: Container(
+                //   color: Colors.red
+                // )
+              ),
+              const Text("s")
+          ]
+        )
+      )
     );
   }
 }
