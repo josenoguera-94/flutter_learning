254e2c552e46de52cd4f691ed378aefcf8f886b1
jose noguera <josenoguera1994@gmail.com>
2024-05-11 19:53:27 -0500

    course(toktik): mapper

--- toktik_videos/lib/entities/video_post.dart
+++ toktik_videos/lib/entities/video_post.dart
@@ -1,13 +1,13 @@
 
 class VideoPost {
 
-  final String caption;
+  final String name;
   final String videoUrl;
   final int likes;
   final int views;
 
   VideoPost({
-    required this.caption,
+    required this.name,
     required this.videoUrl,
     this.likes = 0,
     this.views = 0,
--- toktik_videos/lib/providers/discorver_provider.dart
+++ toktik_videos/lib/providers/discorver_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toktik_videos/entities/videos_post.dart';
+import 'package:toktik_videos/entities/video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
 
